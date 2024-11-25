e5d96fa6484972a7489273b9b4c944542a3c99a0
chaitanyawargand <chaitanyawargand@gmail.com>
2024-11-26 01:19:40 +0530

    Update array19_1.c

--- array19_1.c
+++ array19_1.c
@@ -1,44 +1,44 @@
 #include<stdio.h>
-int main()
-{
+int main(){
     int i,j,k,t,n,flag=0;
-    printf("enter the number of elements\n");
+    printf("Enter the number of elements: ");
     scanf("%d",&n);
-    printf("input the value to be inserted\n");
+    printf("Input the value to be inserted: ");
     scanf("%d",&k);
-    int a[n];
-    printf("enter the elements\n");
-    for(i=0;i<n-1;i++)
-    scanf("%d",&a[i]);
-     for(i=0;i<n-1;i++)
-    {
-        for(j=0;j<n-1;j++)
-        {   if(a[j]>a[j+1])
-           { t=a[j];
-            a[j]=a[j+1];
-            a[j+1]=t;
-           }
+    
+    int a[n+1];
+    printf("Enter the elements: ");
+    
+    for(i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    for(i =0;i<n-1;i++) {
+        for(j=0;j<n-1-i;j++) {
+            if(a[j] > a[j + 1]) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+            }
         }
     }
-    int b[(n+1)];
-    for(i=0;i<n;i++)
-    {
-       if(a[i]<k)
-       {b[i]=a[i];
-       flag++;}
+    for(i=0;i<n;i++) {
+        if(a[i] < k) {
+            a[i] = a[i];
+            flag++;
+        } else {
+            break;
+        }
     }
-    b[flag]=k;
-    for(i<(n+1);i>flag;i--)
-    {
-        a[i]=a[i+1];
+    for(i=n;i>flag;i--) {
+        a[i] = a[i-1];
+    }
+    a[flag] = k;
+
+ printf("Array after insertion: ");
+    for(i =0;i<n+1;i++) {
+        printf("%d ", a[i]);
     }
 
-    for(i=0;i<n;i++)
-    {
-        printf("%d",b[i]);
-    }
     return 0;
-    
-
 }
-        
+             
